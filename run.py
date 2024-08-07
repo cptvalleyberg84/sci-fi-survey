58b72b6ad74a6d4a4f7ec7900459016f5e098f33
Rafal Zygula <p0hmgxcn@students.codeinstitute.net>
2024-08-07 19:30:46 +0000

    Setup initial terminal menu

--- run.py
+++ run.py
@@ -15,6 +15,17 @@ SHEET = GSPREAD_CLIENT.open('sci-fi-data')
 
 sheet1 = SHEET.worksheet('Sheet1')
 
-data = sheet1.get_all_values()
+# data = sheet1.get_all_values()
 
-print(data)
+# print(data)
+
+
+
+def main():
+    options = ['Sci-Fi Survey', 'Data & Results', 'About', 'Exit']
+    terminal_menu = TerminalMenu(options)
+    menu_entry_index = terminal_menu.show()
+    print(f"You've chosen {options[menu_entry_index]}!")
+
+if __name__ == '__main__':
+    main()
