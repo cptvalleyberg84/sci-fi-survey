cd42b23e196fcc7566648a1bc3dc7f4900ab307b
Rafal Zygula <p0hmgxcn@students.codeinstitute.net>
2024-08-12 15:30:59 +0000

    Set up panda dataframes with google sheets

--- run.py
+++ run.py
@@ -4,6 +4,7 @@ from simple_term_menu import TerminalMenu
 import datetime
 import os
 import platform
+import pandas as pd
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -18,8 +19,20 @@ SHEET = GSPREAD_CLIENT.open('sci-fi-data')
 
 sheet1 = SHEET.worksheet('Sheet1')
 
+# Fetch all data from the Google Sheets from Sheet1
+data = sheet1.get_all_values()
+
+# Convert the data into a Pandas DataFrame
+df = pd.DataFrame(data, columns=[
+    "Timestamp",
+    "Age",
+    "Sci-Fi Type",
+    "Likes Speculative Fiction",
+    "Engangement Frequency",
+    "Favourite Sci-Fi Medium"
+])
+
 # Print All Data from the Google Sheets file
-# data = sheet1.get_all_values()
 # print(data)
 
 def greetings_msg():
@@ -51,7 +64,8 @@ def main():
     if menu_entry_index == 0:
         sci_fi_survey()
     elif menu_entry_index == 1:
-        print("Data Results")
+        data_results()
+        # print("Data Results")
     elif menu_entry_index == 2:
         print("Made by Valleyberg")
     elif menu_entry_index == 3:
@@ -93,20 +107,50 @@ def  sci_fi_survey():
     clear_screen()
 
     #Question 3:
-    yes_no_options = [
+    spec_fi_options = [
         "Yes", 
         "No"
         ]
     
-    yes_no_menu = TerminalMenu(yes_no_options, title = "\n3. Do you like speculative fiction?")
-    yes_no_choice_index = yes_no_menu.show()
-    speculative_ficiton = yes_no_options[yes_no_choice_index]
+    spec_fi_menu = TerminalMenu(spec_fi_options, title = "\n3. Do you like speculative fiction?")
+    spec_fi_choice_index = spec_fi_menu.show()
+    speculative_ficiton = spec_fi_options[spec_fi_choice_index]
+
+    clear_screen()
+
+    #Question 4:
+    sci_fi_freq_options = [
+        "Daily",
+        "Weekly",
+        "Monthly",
+        "All the time!"
+        ]
+
+    sci_fi_freq_menu = TerminalMenu(sci_fi_freq_options, title="\n4. How often do you engage with Sci-Fi content?")
+    sci_fi_freq_choice_index = sci_fi_freq_menu.show()
+    sci_fi_freq = sci_fi_freq_options[sci_fi_freq_choice_index]
+
+    clear_screen()
+
+    #Question 5:
+    sci_fi_medium_options = [
+        "Books",
+        "Movies",
+        "TV Shows",
+        "Video Games"
+    ]
+
+    sci_fi_medium_menu = TerminalMenu(sci_fi_medium_options, title="\n5. When getting into Sci-Fi you prefer to use:")
+    sci_fi_medium_choice_index = sci_fi_medium_menu.show()
+    sci_fi_medium = sci_fi_medium_options[sci_fi_medium_choice_index]
+    
+    clear_screen()
 
     #Current TimeStamp
     timestamp =  str(datetime.datetime.now())
 
     #Prepare the data to be stored
-    survey_data = [timestamp, age, sci_fi_type, speculative_ficiton]
+    survey_data = [timestamp, age, sci_fi_type, speculative_ficiton, sci_fi_freq, sci_fi_medium]
 
     clear_screen()
 
@@ -114,6 +158,8 @@ def  sci_fi_survey():
     print(f"\nAge: {survey_data[1]}")
     print(f"Preferred Sci-Fi Type: {survey_data[2]}")
     print(f"Like Speculative Fiction: {survey_data[3]}")
+    print(f"Engangement Frequency: {survey_data[4]}")
+    print(f"Favourite Sci-Fi Medium: {survey_data[5]}")
     print(f"\n{survey_data[0]}")
 
 
@@ -123,6 +169,10 @@ def  sci_fi_survey():
 
 # def store_survey_data(survey_data):
 #     sheet1.append_row(survey_data)
+
+def data_results():
+    print(df.head())
+
 
 def clear_screen():
     """Clears the terminal screen so it's all nice and neat."""
